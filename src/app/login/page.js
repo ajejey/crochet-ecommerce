049e5d2fbe8a208c0372bd18b1a0924f9a4981da
Ajey <ajejey@gmail.com>
2025-04-08 23:59:58 +0530

    fix login page height in mobile

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,7 +3,7 @@ import HeaderSection from "../(header)/HeaderSection";
  
 export default function LoginPage({ searchParams }) {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    <main className="flex items-center justify-center h-screen">
       <HeaderSection />
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <LoginForm searchParams={searchParams} />
